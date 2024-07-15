85d78b0b2e8cd9291c01a91ebd0f57d745be9a9b
Nombali Mafa <nombalimafa@gmail.com>
2024-07-15 22:48:25 +0200

    Updated Welcome

--- KhulisaMobile/app/screens/WelcomeScreen.js
+++ KhulisaMobile/app/screens/WelcomeScreen.js
@@ -1,28 +1,66 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const WelcomeScreen = ({ navigation }) =>{
     return(
-        <View style={StyleSheet.container}>
-            <Image source={require('../../assets/KhulisaLogo1.jpg')} style={StyleSheet.logo}/>
+        <View style={styles.container}>
+            <LinearGradient
+        colors={['#7AE844', 'rgb(0, 140, 39)']}
+        locations={[0, 1]}
+        start={{ x: 0, y: 0.5 }}
+        end={{ x: 1, y: 0.5 }}
+        style={styles.linearGradient}
+      >
+       <Image source={require('../../assets/KHULISA1-removebg-preview.png')} style={styles.logo}/>
+       <Text style={styles.text}>Planting Seeds of Growth</Text>
+       <TouchableOpacity style={styles.button}><Text style={styles.buttonText}>Get Started</Text></TouchableOpacity>
+      </LinearGradient>
+        
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     body:{
-        backgroundColor:'white'
+        // backgroundColor:'red'
+       
+    },
+    buttonText:{
+        color:'white',
     },
     container: {
-        backgroundColor:'white',
+        fontFamily: 'League Spartan',
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
       },
+      text:{
+        fontFamily: 'League Spartan',
+        fontSize: 18,
+        color: 'white',
+        top: -170,
+        fontWeight: 'bold',
+
+      },
       logo: {
-        width: 200,
-        height: 200, 
+        width: 450,
+        height: 500, 
         resizeMode: 'contain', 
+        marginTop: 40,
+      },
+      linearGradient: {
+        flex: 1,
+        alignItems: 'center',
+        height: 100,
+        width: '100%',
+      },
+      button: {
+        backgroundColor: 'rgb(0, 140, 39)',
+        top: -110,
+        padding: 15,
+        borderRadius: 20,
+        width: 150,
+        alignItems: 'center',
       },
 });
 
