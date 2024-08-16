4c651df725e12794fbfd631e33213856b69d2070
NokuhleNgubane <ngubanenokuhle@gmail.com>
2024-08-16 21:22:54 +0200

    changes to climate screen

--- KhulisaMobile/app/screens/ClimateAlertsScreen.js
+++ KhulisaMobile/app/screens/ClimateAlertsScreen.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, ImageBackground, StyleSheet, TextInput, Keyboard, TouchableWithoutFeedback, ScrollView, KeyboardAvoidingView, Button } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { requestLocationPermission, getCurrentLocation } from '../../components/LocationData';
+import { getWeatherData } from '../../components/WeatherData';
 import MapView, { Marker } from 'react-native-maps';
 import axios from 'axios';
 
@@ -8,156 +10,114 @@ import axios from 'axios';
 
 const ClimateAlertScreen = () => {
     const [location, setLocation] = useState('');
-    const [region, setRegion] = useState(null);
+    const [weatherData, setWeatherData] = useState(null);
 
-    const fetchCoordinates = async (city) => {
-        try {
-            const apiKey = 'ed9b0e77ad2f488e82e7dc15406c03bb'; // Replace with your OpenCage API key
-            const response = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${apiKey}`);
-            const data = response.data;
-            if (data.results.length > 0) {
-                const { lat, lng } = data.results[0].geometry;
-                fetchWeatherData({ lat, lon: lng });
-            } else {
-                console.error('No results found for the specified city.');
-            }
-        } catch (error) {
-            console.error('Error fetching coordinates:', error);
+    useEffect(() => {
+        (async () => {
+        const hasPermission = await requestLocationPermission();
+        if (!hasPermission) {
+            return;
+        }
+        })();
+    }, []);
+
+    const getLocation = async () => {
+        const coords = await getCurrentLocation();
+        if (coords) {
+        const { latitude, longitude } = coords;
+        setLocation(`${latitude}, ${longitude}`);
+        fetchWeatherData(latitude, longitude); 
         }
     };
 
-    const fetchWeatherData = async (location) => {
+    const fetchWeatherData = async (latitude, longitude) => {
         try {
-            const apiKey = 'Dv5dNbICLCyGfggSlgUEIjSV3yyPPfXp'; // Replace with your Windy API key
-            const response = await fetch(`https://api.windy.com/api/point-forecast/v2?lat=${location.lat}&lon=${location.lon}&key=${apiKey}`);
-            
-            if (!response.ok) {
-                console.error(`HTTP error! status: ${response.status}`);
-                const textResponse = await response.text();
-                console.error('Error response:', textResponse);
-                return;
-            }
-            
-            const data = await response.json();
-            setRegion({
-                latitude: location.lat,
-                longitude: location.lon,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            });
+        const data = await getWeatherData(latitude, longitude);
+        console.log('Weather Data:', data);
+        setWeatherData(data);
         } catch (error) {
-            console.error('Error fetching weather data:', error);
+        Alert.alert('Error', 'Unable to fetch weather data');
         }
     };
-    
-
-    const handleLocationSubmit = () => {
-        fetchCoordinates(location);
-    };
-
-    const dismissKeyboard = () => {
-        Keyboard.dismiss();
-    };
+   
 
     return (
-        <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
-            <ScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled">
-                <TouchableWithoutFeedback onPress={dismissKeyboard}>
-                    <ImageBackground 
-                        source={require('../../assets/cyclone.jpg')} 
-                        style={styles.background} 
-                        imageStyle={{ opacity: 0.7 }}
-                    >
-                        <View style={styles.overlay}>
-                            
-                            <View style={styles.search}>
-                                <TextInput
-                                    style={styles.input}
-                                    value={location}
-                                    onChangeText={setLocation}
-                                    placeholder="Enter your city"
-                                />
-                                <Icon name="search" size={30} color="#008c27" style={styles.search_icon} onPress={handleLocationSubmit} />
-                            </View>
-                           
-                            {region && (
-                                <MapView
-                                    style={styles.map}
-                                    region={region}
-                                >
-                                    <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
-                                </MapView>
-                            )}
-                        </View>
-                    </ImageBackground>
-                </TouchableWithoutFeedback>
-            </ScrollView>
-
-        </KeyboardAvoidingView>
+        <ScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled">
+            <ImageBackground 
+                source={require('../../assets/cyclone.jpg')} 
+                style={styles.background} 
+                imageStyle={{ opacity: 0.7 }}>
+                <Button title='Get Current Location' onPress={getLocation} />
+                <TextInput value={location} placeholder="Location coordinates" editable={false} style={styles.textinputbox}/>
+                {weatherData && (
+                    <View style={styles.information_container}>
+                    <View style={styles.row}>
+                    <Text style={styles.label}>Wind:</Text>
+                    <Text style={styles.value}>{weatherData['wind-surface'] ? weatherData['wind-surface'][0] : 'N/A'} m/s</Text>
+                    </View>
+                    <View style={styles.row}>
+                    <Text style={styles.label}>Dew Point:</Text>
+                    <Text style={styles.value}>{weatherData['dewpoint-surface'] ? weatherData['dewpoint-surface'][0] : 'N/A'} °C</Text>
+                    </View>
+                    <View style={styles.row}>
+                    <Text style={styles.label}>Humidity:</Text>
+                    <Text style={styles.value}>{weatherData['rh-surface'] ? weatherData['rh-surface'][0] : 'N/A'} %</Text>
+                    </View>
+                    <View style={styles.row}>
+                    <Text style={styles.label}>Pressure:</Text>
+                    <Text style={styles.value}>{weatherData['pressure-surface'] ? weatherData['pressure-surface'][0] : 'N/A'} hPa</Text>
+                    </View>
+                    <View style={styles.row}>
+                    <Text style={styles.label}>Precipitation:</Text>
+                    <Text style={styles.value}>{weatherData['past3hprecip-surface'] ? weatherData['past3hprecip-surface'][0] : 'N/A'} mm</Text>
+                    </View>
+                </View>
+                )}
+            </ImageBackground>
+      
+    </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+      flex: 1,
     },
     background: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
+      flex: 1,
+      width: '100%',
+      height: '100%',
     },
-    overlay: {
-        flex: 1,
+    textinputbox:{
+      borderColor: 'black', 
+      borderWidth: 3, 
+      Tcolor:'black',
+      marginTop: 10, 
+      padding: 5
     },
-    bars: {
-        marginLeft: 15,
+    information_container: {
+      marginTop: 20,
+      backgroundColor: 'rgba(255, 255, 255, 0)', // Clear background
+      paddingHorizontal: 10,
     },
-    heading: {
-        fontSize: 22,
-        color: 'rgb(0,140,39)',
-        fontWeight: 'bold',
-        marginLeft: 40,
+    row: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingVertical: 8,
+      borderBottomWidth: 1,
+      borderBottomColor: 'rgba(255, 255, 255, 0.5)', // Light separator line
     },
-    head: {
-        paddingBottom: 15,
-        paddingTop: 15,
-        width: '100%',
-        backgroundColor: 'white',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        display: 'flex',
-        flexDirection: 'row',
+    label: {
+      fontWeight: 'bold',
+      color: 'white',
+      backgroundColor: 'rgba(255, 255, 255, 0.3)', // White background with lower opacity
+      paddingHorizontal: 8,
+      paddingVertical: 4,
+      borderRadius: 4,
     },
-    input: {
-        height: 40,
-        borderColor: '#008C27',
-        borderWidth: 2,
-        top: 20,
-        paddingHorizontal: 8,
-        width: 260,
-        borderRadius: 10,
-        backgroundColor: 'white',
-        marginLeft: 35,
+    value: {
+      color: 'white',
     },
-    search: {
-        display: 'flex',
-        flexDirection: 'row',
-    },
-    search_icon: {
-        marginLeft: 10,
-        top: 25,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-    },
-    map: {
-        width: '100%',
-        height: 300,
-        marginTop: 20,
-    },
-});
+  });
 
 export default ClimateAlertScreen;
